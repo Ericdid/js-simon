5143dc3f5d0cd00b14c9434f17e09a08444b5a4b
Ericdid <ericdidom@gmail.com>
2023-08-23 17:23:49 +0200

    add.html-elements

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 // ---------Inserisco la data da ragiungere
-let cdTarget = new Date("Aug 24, 202 09:30:00").getTime();
+const cdTarget = new Date("Aug 24, 2023 09:30:00").getTime();
 
 // ---------Inserisco la data attuale
 let start = Date.now();
 
-console.log(start);
-console.log(cdTarget);
-
 //----------Inserisco la differenza
 let timeLeft = cdTarget - start;
-console.log(timeLeft);
+
+//----------Converto il tempo
+let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+//----------Inserisco i Dati negli appositi spazi in HTML
+document.getElementById(`days`).innerHTML = days;
+document.getElementById(`hours`).innerHTML = hours;
+document.getElementById(`minutes`).innerHTML = minutes;
+document.getElementById(`seconds`).innerHTML = seconds;
+
+//----------Inizializzo il Countdown
